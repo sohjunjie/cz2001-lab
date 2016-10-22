18d1e4b76b4b75a11f0c582d8a6f289d0ca435e2
sohjunjie <junjie.soh93@gmail.com>
2016-10-22 10:44:55 +0800

    run create new graphset in main method

--- GraphLab4/src/GraphTestApp.java
+++ GraphLab4/src/GraphTestApp.java
@@ -3,6 +3,9 @@ public class GraphTestApp {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		GraphSet GraphSet1 = new GraphSet(5000);
+//		GraphSet GraphSet2 = new GraphSet(10000);
 
 	}
 	
